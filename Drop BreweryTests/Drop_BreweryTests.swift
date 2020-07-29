6d804c4ccc7cd9cd53a936ccbd666cd3f51c3731
Conor Nolan <IAmConorNolan@gmail.com>
2020-07-29 18:33:52 +0100

    Initial Commit

---
+++ Drop BreweryTests/Drop_BreweryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Drop_BreweryTests.swift
+//  Drop BreweryTests
+//
+//  Created by Conor Nolan on 29/07/2020.
+//  Copyright © 2020 Conor Nolan. All rights reserved.
+//
+
+import XCTest
+@testable import Drop_Brewery
+
+class Drop_BreweryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
